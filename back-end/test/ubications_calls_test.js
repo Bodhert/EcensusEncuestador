432e5e7cc9c971e9a2da3a508c910cf38742c2d9
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-11-04 21:47:54 -0500

    simple tests done

--- back-end/test/ubications_calls_test.js
+++ back-end/test/ubications_calls_test.js
@@ -2,16 +2,41 @@
 const request = require('supertest');
 const assert = require('assert');
 const app = require('../index');
+const Ubication = require('../models/ubication');
 
-describe('GET /ubications', function(){
-    it('should bring all the ubications',function(done){
+//TODO: remember to save ubications before testing
+
+describe('GET /ubications', function () {
+
+    beforeEach(function (done) {
+
+        var ubication = new Ubication({
+            latitude: "3333",
+            longitude: "4444"
+        });
+
+        ubication.save().then(function () {
+            var ubication2 = new Ubication({
+                latitude: "5454",
+                longitude: "6777"
+            });
+
+            ubication2.save().then(function () {
+                done();
+            });
+        });
+
+
+    });
+
+    it('should bring all the ubications', function (done) {
         request(app)
-        .get('/ubications')
-        .expect(200)
-        .expect('Content-Type','application/json; charset=utf-8')
-        .expect(function(res){
-            assert.equal(res.body['0']['latitude'], '3333')
-        })
-        .end(done);
+            .get('/ubications')
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                assert.notEqual(res.body.length, 0);
+            })
+            .end(done);
     })
 });
